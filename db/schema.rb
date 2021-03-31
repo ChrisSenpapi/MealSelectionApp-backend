86e7655c48363975f865b60af5bf2ef37d9766fb
Christopher Read <Christopherread22@outlook.com>
2021-03-30 20:38:42 -0400

    rails done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_230740) do
+ActiveRecord::Schema.define(version: 2021_03_31_001829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "meal_ingredients", force: :cascade do |t|
+    t.bigint "ingredient_id", null: false
+    t.bigint "meal_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_meal_ingredients_on_ingredient_id"
+    t.index ["meal_id"], name: "index_meal_ingredients_on_meal_id"
+  end
 
   create_table "meals", force: :cascade do |t|
     t.string "name"
@@ -22,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_03_30_230740) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "meal_ingredients", "ingredients"
+  add_foreign_key "meal_ingredients", "meals"
 end
