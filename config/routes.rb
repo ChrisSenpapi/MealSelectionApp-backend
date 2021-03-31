8dfe9eec903bbcb8bacd9f844e7919fd2bdff748
Christopher Read <Christopherread22@outlook.com>
2021-03-31 17:24:47 -0400

    seed fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :meal_ingredients
   resources :ingredients
+  resources :meals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :meals
+      resources :meals 
     end
   end
 end
